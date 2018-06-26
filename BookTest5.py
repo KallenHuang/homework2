76bfff47e284c84cfb04b1031043ac1ebb549502
kallenhuang <kallen1981@gmail.com>
2018-06-26 11:25:06 +0800

    測試

---
+++ BookTest5.py
@@ -0,0 +1,15 @@
+#建立一個串列
+capture=["one","two","three","fure","one"]
+print(capture)
+print(capture[2])
+
+print(capture.count("one"))
+print(len(capture))
+
+list1=list("seven")
+print(max(list1))
+
+print("two" in capture)
+
+FaceIndex={"one":70,"two":60,"three":87,"four":65}
+print(FaceIndex["two"])
